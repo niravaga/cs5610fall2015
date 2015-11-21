be08cb195384f364e03f91a29b6dfdef36a8ada5
Nirav Aga <niravaga@gmail.com>
2015-11-20 18:21:04 -0800

    Openshift config

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/cs5610example');
-var db = mongoose.connection;
 
 
 app.use(express.static(__dirname + '/public'));
-// app.use(express.bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var mongo_url = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/cs5610example';
+mongoose.connect(mongo_url);
+var db = mongoose.connection;
 
 
 require("./public/experiments/mongo/server/app.js")(app, mongoose, db);
